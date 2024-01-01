782b0fe714f4565806aaf197af862e2f98d51f85
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-01-01 12:56:03 +0300

    Updates

---
+++ assets/ConteinerInfo-aa4a2644.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-bcdb6f9b.js";const s="_conteinerInfo_k3bsd_1",e={conteinerInfo:s},r=({children:o})=>n.jsx("div",{className:e.conteinerInfo,children:n.jsx("div",{className:e.wrapper,children:o})});export{r as default};
